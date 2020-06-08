56aa2a3b70247bd852e24ed23197a62827141cb4
Lucien <lucienmartijn@gmail.com>
2020-06-08 13:19:23 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,20 @@
 
 set -e
 
-# Extract the base64 encoded config data and write this to the KUBECONFIG
-echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
-export KUBECONFIG=/tmp/config
+main(){
+  # Extract the base64 encoded config data and write this to the KUBECONFIG
+  echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
+  export KUBECONFIG=/tmp/config
+  
+  
+  if uses "${INPUT_NAMESPACE}"; then
+     echo "${INPUT_USERNAME}"
+     sh -c "kubectl -n '${INPUT_NAMESPACE}' $*"
+  else
+     echo "no name space provided"
+     sh -c "kubectl $*"
+  fi
+}
 
-sh -c "kubectl $*"
+main
+
